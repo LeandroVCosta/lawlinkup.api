7568e24c40a2525653023bf89e0696740310c94b
Gabriel Felipe Silva Macedo <gabriel.macedo@vericode.com.br>
2023-08-29 20:37:01 -0300

    alinhando com regras de negocio do cadastro

--- Projeto lawLinkup/Projeto-lawLinkup/src/main/kotlin/lawlinkup/Projeto/lawLinkup/infra/security/SecurityConfiguration.kt
+++ Projeto lawLinkup/Projeto-lawLinkup/src/main/kotlin/lawlinkup/Projeto/lawLinkup/infra/security/SecurityConfiguration.kt
@@ -21,7 +21,7 @@ class SecurityConfiguration {
                 .and()
 //                .cors().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/usuario/**", "/cliente/**", "/caso/**", "/vinculo/**", "/login/**", "/advogado/**", "/docs/**", "/swagger-ui.html/**","/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui/index.html/**")
+                .requestMatchers("/usuario/**", "/cliente/**", "/caso/**", "/vinculo/**", "/login/**", "/advogado/**", "/tipoUsuario/**", "/docs/**", "/swagger-ui.html/**","/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui/index.html/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
